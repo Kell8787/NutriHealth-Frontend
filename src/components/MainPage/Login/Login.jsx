b69ac0bd2bbb0462e614d4bb5419ad8ebc93ab1f
iamcarlogg <00052220@uca.edu.sv>
2023-12-04 19:15:49 -0600

    arreglando login v1

--- src/components/MainPage/Login/Login.jsx
+++ src/components/MainPage/Login/Login.jsx
@@ -25,7 +25,7 @@ function Login({ onClose }) {
       
       // Redirigir al usuario a otra página después del login exitoso
       window.location.href = '/';
-      setIsOpen(!isOpen);
+      setIsOpen(onClose);
      
     } catch (error) {
       console.error('Error en el login:', error);
